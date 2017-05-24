1c93e25c32a4bbe8365fcc74b96fed46f01a02e8
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-05-24 21:54:48 +0200

    Marshal track fields

--- whippet_api/resources/track_resource.py
+++ whippet_api/resources/track_resource.py
@@ -1,11 +1,20 @@
-from flask_restful import Resource
+from flask_restful import fields, marshal_with, Resource
 from flask_jwt import jwt_required
 
 from whippet_api.models import Track
 
 
+track_fields = {
+    'id': fields.Integer,
+    'artist_name': fields.String,
+    'title': fields.String,
+    'release': fields.String
+}
+
+
 class TrackResource(Resource):
     method_decorators = [jwt_required()]
 
+    @marshal_with(track_fields)
     def get(self):
         return Track.query.all()
