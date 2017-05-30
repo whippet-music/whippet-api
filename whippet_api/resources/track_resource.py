7cbec53765616c01a191c17c32e213b0e76ec1ca
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-05-31 00:43:23 +0200

    Add filtering tracks by id

--- whippet_api/resources/track_resource.py
+++ whippet_api/resources/track_resource.py
@@ -1,4 +1,4 @@
-from flask_restful import fields, marshal_with, Resource
+from flask_restful import reqparse, fields, marshal_with, Resource
 from flask_jwt import jwt_required
 
 from whippet_api.models import Track
@@ -11,10 +11,19 @@ track_fields = {
     'release': fields.String
 }
 
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=int,
+                          action='append',
+                          location='args')
+
 
 class TrackResource(Resource):
     method_decorators = [jwt_required()]
 
     @marshal_with(track_fields)
     def get(self):
-        return Track.query.limit(10).all()
+        args = parser.parse_args()
+        if 'id' in args and not args['id'] == None:
+            return Track.query.filter(Track.id.in_(args['id'])).all()
+        else:
+            return Track.query.limit(10).all()
