16de04718065cfe7344308d227568a793201b78f
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-05-30 21:51:21 +0200

    Generate random recommendations

--- whippet_api/resources/recommendation_resource.py
+++ whippet_api/resources/recommendation_resource.py
@@ -1,14 +1,19 @@
-from flask_restful import Resource
+from flask_restful import fields, marshal_with, Resource
 from flask_jwt import jwt_required
+from sqlalchemy  import func, desc
 
-RECOMMENDATIONS = [
-    {'id': 1, 'name': 'Smoke on the water', 'artist': 'Deep Purple'},
-    {'id': 1, 'name': 'Smoke on the water', 'artist': 'Deep Purple'},
-    {'id': 1, 'name': 'Smoke on the water', 'artist': 'Deep Purple'}
-]
+from whippet_api.models import Track
+
+
+recommendation_fields = {
+    'track_id': fields.Integer
+}
+
 
 class RecommendationResource(Resource):
     method_decorators = [jwt_required()]
 
+    @marshal_with(recommendation_fields)
     def get(self):
-        return RECOMMENDATIONS
+        tracks = Track.query.order_by(func.random()).limit(10).all()
+        return [{'track_id': track.id} for track in tracks]
