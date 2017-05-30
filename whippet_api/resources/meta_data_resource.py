f21ede23c37d24b6ff16021bce1dad5ee1dd8af8
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-05-30 21:17:31 +0200

    Serialize meta data objects

--- whippet_api/resources/meta_data_resource.py
+++ whippet_api/resources/meta_data_resource.py
@@ -1,11 +1,37 @@
-from flask_restful import Resource
+from flask_restful import fields, marshal_with, Resource
 from flask_jwt import jwt_required
 
 from whippet_api.models import MetaData
 
 
+meta_data_fields = {
+    'id': fields.Integer,
+    'track_id': fields.Integer,
+    'year': fields.Integer,
+    'artist_familiarity': fields.Float,
+    'artist_hotness': fields.Float,
+    'artist_latitude': fields.Float,
+    'artist_longitude': fields.Float,
+    'duration': fields.Float,
+    'end_of_fade_in': fields.Float,
+    'key': fields.Integer,
+    'key_confidence': fields.Float,
+    'loudness': fields.Float,
+    'mode': fields.Integer,
+    'mode_confidence': fields.Float,
+    'song_hotness': fields.Float,
+    'start_of_fade_out': fields.Float,
+    'tempo': fields.Float,
+    'time_signature': fields.Integer,
+    'time_signature_confidence': fields.Float,
+    'created_at': fields.DateTime,
+    'updated_at': fields.DateTime
+}
+
+
 class MetaDataResource(Resource):
     method_decorators = [jwt_required()]
 
+    @marshal_with(meta_data_fields)
     def get(self):
-        return MetaData.query.all()
+        return MetaData.query.limit(10).all()
