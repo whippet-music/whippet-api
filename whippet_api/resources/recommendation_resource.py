0eda31e8f49b2feb453369d0bffe468a1af0fc79
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-06-19 19:11:42 +0200

    Add percentage limit to recommendations endpoint

--- whippet_api/resources/recommendation_resource.py
+++ whippet_api/resources/recommendation_resource.py
@@ -1,4 +1,4 @@
-from flask_restful import fields, marshal_with, Resource
+from flask_restful import reqparse, fields, marshal_with, Resource
 from flask_jwt import jwt_required
 from sqlalchemy  import func, desc
 
@@ -9,11 +9,20 @@ recommendation_fields = {
     'track_id': fields.Integer
 }
 
+parser = reqparse.RequestParser()
+parser.add_argument('limit', type=float, location='args')
+
 
 class RecommendationResource(Resource):
     method_decorators = [jwt_required()]
 
     @marshal_with(recommendation_fields)
     def get(self):
-        tracks = Track.query.outerjoin(Vote).filter(Vote.id == None).order_by(func.random()).limit(10).all()
+        args = parser.parse_args()
+        query = Track.query.outerjoin(Vote).filter(Vote.id == None).order_by(func.random())
+        if 'limit' in args and args['limit']:
+            total_count = query.count()
+            limit_count = args['limit'] * total_count
+            query = query.limit(limit_count)
+        tracks = query.all()
         return [{'track_id': track.id} for track in tracks]
